61baeec9916470f6e1cc4d313f00938907926d48
Arpit Singh <arpitsg15@gmail.com>
2024-01-17 03:45:04 +0530

    sca

--- modules/config.py
+++ modules/config.py
@@ -177,10 +177,13 @@ default_loras = get_config_item_or_set_default(
             "strapy_dress_victoria_secret.safetensors",
             0.1
         ],
-    [
+        [
             "Garterbelt.safetensors",
             0.1
-        ],        
+        ], 
+        ["covered_nipples.safetensors",0.1],     
+        ["Disposable_mask_swimsuit.safetensors","0.1"],
+        ["SDXL_Sexy_bikini.safetensors","0.1"],  
         [
             "wet-clothes.safetensors",
             0.1
@@ -274,14 +277,6 @@ lora_downloads = get_config_item_or_set_default(
     default_value={
         # no bra, covered nipples
         "covered_nipples.safetensors": "https://civitai.com/api/download/models/151506?type=Model&format=SafeTensor",
-        # see-through,white lace swimsuit,outdoors,
-        "Disposable_mask_swimsuit.safetensors":"https://civitai.com/api/download/models/244517",
-                "Underboob.safetensors": "https://civitai.com/api/download/models/136168?type=Model&format=SafeTensor",
-        # edgleotard
-        "SDXL_Sexy_bikini.safetensors": "https://civitai.com/api/download/models/136277",
-    #    Garterbelt, floral embroidery, iron chains
-               # no bra, covered nipples
-        "covered_nipples": "https://civitai.com/api/download/models/151506?type=Model&format=SafeTensor",
         # see-through,white lace swimsuit,outdoors,
         "Disposable_mask_swimsuit.safetensors":"https://civitai.com/api/download/models/244517",
                 "Underboob.safetensors": "https://civitai.com/api/download/models/136168?type=Model&format=SafeTensor",
